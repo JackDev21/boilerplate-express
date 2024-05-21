e3e6e052388cd6b072564709882f0f7c5a3db86f
Jose A. Cantó (Jack) <jose.a.c.lopez@gmail.com>
2024-05-21 16:07:05 +0000

    update app.get

--- myApp.js
+++ myApp.js
@@ -1,45 +1,27 @@
 let express = require('express');
 let app = express();
 
-
-
-
 console.log("Hello World");
+
+
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
 
 
 app.get("/", (req, res) => {
 
   const absolutePath = __dirname + "/views/index.html"
 
+  
   res.sendFile(absolutePath);
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/json", (req, res) =>{
+  res.json({
+    message: "Hello json"
+  })
+})
 
 
 
